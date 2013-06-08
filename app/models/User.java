d0d61bce8c4c68336b7b85687ae385431cfc0b2b
Boris Kalchev <kalchev@in.tum.de>
2013-06-08 12:16:23 +0200

    fjdsa

---
+++ app/models/User.java
@@ -0,0 +1,103 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sushil
+ * Date: 6/8/13
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+public class User extends Model {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean isAdmin;
+    private String dob;
+    private String phone;
+    private String occupation;
+    private String description;
+
+
+    public User(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
