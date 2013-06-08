dfdfdd2d7cee35841e0400cd9aeccf1e2124b7c9
Sushil <sushilman@gmail.com>
2013-06-08 11:45:43 +0200

    Added model

---
+++ app/models/User.java
@@ -0,0 +1,18 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sushil
+ * Date: 6/8/13
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+public class User extends Model {
+
+}
