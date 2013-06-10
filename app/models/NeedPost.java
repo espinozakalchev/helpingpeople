94e619091779e6af2aed4bac73b7b3fab7012869
Boris Kalchev <kalchev@in.tum.de>
2013-06-10 09:40:46 +0200

    change to skeleton

--- app/models/NeedPost.java
+++ app/models/NeedPost.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+
+import play.db.jpa.Blob;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +16,71 @@ import javax.persistence.Entity;
 
 @Entity
 public class NeedPost extends Post {
+    private Blob picture;
+    private String description;
+    private User user;
+    private Date createdDate;
+    private Date modifiedDate;
+    
+    private boolean closed = false;
 
-    public NeedPost(){
+	public NeedPost(Blob picture, String description, User user,
+			Date createdDate, Date modifiedDate) {
+		super();
+		this.picture = picture;
+		this.description = description;
+		this.user = user;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+		
+		create();
+	}
 
-    }
+	public Blob getPicture() {
+		return picture;
+	}
 
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public boolean isClosed() {
+		return closed;
+	}
+
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+	}
 }
