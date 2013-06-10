989e156f68d99bef1b2fc2aa3f09dff0899e7fc1
Boris Kalchev <kalchev@in.tum.de>
2013-06-10 09:40:13 +0200

    change to skeleton

--- app/models/CanPost.java
+++ app/models/CanPost.java
@@ -1,6 +1,9 @@
 package models;
 
 import javax.persistence.Entity;
+
+import play.db.jpa.Blob;
+
 import java.util.Date;
 
 /**
@@ -13,10 +16,71 @@ import java.util.Date;
 
 @Entity
 public class CanPost extends Post {
+    private Blob picture;
     private String description;
     private User user;
     private Date createdDate;
     private Date modifiedDate;
-    private boolean isDeleted;
+    
+    private boolean closed = false;
 
+	public CanPost(Blob picture, String description, User user,
+			Date createdDate, Date modifiedDate) {
+		super();
+		this.picture = picture;
+		this.description = description;
+		this.user = user;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+		
+		create();
+	}
+
+	public Blob getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public boolean isClosed() {
+		return closed;
+	}
+
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+	}
 }
