6252a61d0b091b81e25da40e3b612bea25bca016
Sushil <sushilman@gmail.com>
2013-06-20 21:58:08 +0200

    Added default administrator user

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,14 +1,19 @@
 import models.CanPost;
+import models.User;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
+
+import java.util.Date;
 
 
 @OnApplicationStart
 public class Bootstrap extends Job {
 
 	public void doJob() {
-		if (CanPost.count() == 0)
+        User user = new User("administrator","admin@helpingpeople.com","password","password", "Admin","admin",new Date(),"123","Developer","Nothing much");
+        if (CanPost.count() == 0)
 			Fixtures.loadModels("posts.yml");
+
 	}
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,7 +12,6 @@ public class Application extends Controller {
     public static void index() {
 		List<CanPost> canPosts = CanPost.find("from CanPost order by createdDate desc").fetch();
 		List<NeedPost> needPosts = NeedPost.find("from NeedPost order by createdDate desc").fetch();
-        //User user = new User("administrator","admin@helpingpeople.com","password","password", "Admin","admin",new Date(),"123","Developer","Nothing much");
         render(canPosts, needPosts);
     }
 
