bd1a4e02d62d12b085cee4bab320f35070d702a5
Moinul Al-Mamun <moinulkuet@gmail.com>
2013-07-09 22:06:36 +0200

    static pages + updated link in main page

---
+++ app/controllers/About.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.With;
+
+public class About extends Controller {
+	
+	public static void index() {
+		render();
+	}
+
+}
---
+++ app/controllers/Terms.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.With;
+
+public class Terms extends Controller {
+	
+	public static void index() {
+		render();
+	}
+
+}
